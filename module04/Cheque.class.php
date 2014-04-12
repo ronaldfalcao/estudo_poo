71562d3b68201259f8719579a7635cf77e564825
ronaldfalcao <ronald@ronaldfalcao.com.br>
2014-04-12 11:53:43 -0300

    Criando comentários nos arquivos do módulo 04

--- module04/Cheque.class.php
+++ module04/Cheque.class.php
@@ -1,4 +1,8 @@
 <?php
+
+    /*Classe base para entender o conceito de polimorfismo
+     * dela será estendida a classe Cheque Especial      
+     */
     class Cheque{
         
         public $valor;
@@ -7,10 +11,12 @@
             $this->valor = $Valor;
         }
         
+        //Função calcularJuros() que será sobrescrita na classe filha.
         function calcularJuros(){
             return $this->valor * 1.20;
         }
         
+        //Função tipoCheque() que será sobrescrita na classe filha.
         function tipoCheque() {
             return "Comum";
         }
