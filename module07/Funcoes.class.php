6a87225b6c49fd3ae5f47d34956ad28ce5582528
ronaldfalcao <ronald@ronaldfalcao.com.br>
2014-04-13 11:03:59 -0300

    Módulo 7

---
+++ module07/Funcoes.class.php
@@ -0,0 +1,9 @@
+<?php
+    
+    /*Definição de uma classe abstrata (não pode ser instanciada) e de uma constante
+     * que será acessada por uma classe filha.
+    */
+    abstract class Funcoes{
+        const PI = 31.141592653589793238462643383279;
+    }
+
